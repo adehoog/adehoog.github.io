3c90749c69250711789c8831ec8fe23147f12b6f
adehoog <adehoog@ufl.edu>
2022-11-07 18:05:17 -0500

    Skills added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "../styles/Home.css";
 import Headshot from "../images/headshot(square).png";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
 import ItchioIcon from "../images/itchio-logo.png";
+import {SiCplusplus, SiCsharp, SiPython, SiJava, SiJavascript, SiUnity, SiAdobephotoshop, SiBlender} from "react-icons/si"
 
 function Home() {
     return(
@@ -11,6 +15,12 @@ function Home() {
                 <h2>
                     Welcome to my portfolio website. I keep information on my current and past coding projects here, as well as other work I've done that's important to me.
                 </h2>
+                <div className={"prompt"}>
+                    <EmailIcon />
+                    <GitHubIcon />
+                    <LinkedInIcon width={100} height={100}/>
+                    <img src={ItchioIcon} alt={"Itch.io"} style={{width: 55, height: 55, marginBottom: 7}}/>
+                </div>
             </div>
             <div className={"row"}>
                 <div className={"profile"}>
@@ -29,7 +39,31 @@ function Home() {
                     </p>
                 </div>
             </div>
-            <div className={"skills"}> </div>
+            <div className={"skills"}>
+                <h1>Skills</h1>
+                <ol className={"list"}>
+                    <li className={"item"}>
+                        <h2> Programming Languages </h2>
+                        <span>C++, C#, Python, Java, Javascript</span>
+                        <div className={"icons"}>
+                            <SiCplusplus />
+                            <SiCsharp />
+                            <SiPython />
+                            <SiJava />
+                            <SiJavascript />
+                        </div>
+                    </li>
+                    <li className={"item"}>
+                        <h2> Game Development </h2>
+                        <span>Unity, Photoshop, Blender</span>
+                        <div className={"icons"}>
+                            <SiUnity />
+                            <SiAdobephotoshop />
+                            <SiBlender />
+                        </div>
+                    </li>
+                </ol>
+            </div>
         </div>
     );
 }
