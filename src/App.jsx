24da0231bc758e7bf69cb5088ff0ecdb5e4c8cec
adehoog <adehoog@ufl.edu>
2025-08-21 21:18:45 -0400

    Navbar working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import Publications from './components/Publications';
 
-function HomeScreen() {
+function HomePage() {
   return (
     <>
+      <Header></Header>
+      <Home></Home>
+    </>
+  )
+}
 
+function ProjectsPage() {
+  return (
+    <>
+      <Header></Header>
+      <Projects></Projects>
+    </>
+  )
+}
+
+function PublicationsPage() {
+  return (
+    <>
+      <Header></Header>
+      <Publications></Publications>
     </>
   )
 }
@@ -14,8 +35,13 @@ function HomeScreen() {
 function App() {
   return (
     <>
-      <Header></Header>
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path={"/"} element={<HomePage/>}/>
+          <Route path={"/projects"} element={<ProjectsPage/>}/>
+          <Route path={"/publications"} element={<PublicationsPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
